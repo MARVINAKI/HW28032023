54f8669877274414a73da09edca55596267f4712
MARVINAKI <kosakbig@gmial.com>
2023-04-02 09:59:26 +0500

    for check

---
+++ src/main/Java/DAO/CityDAO.java
@@ -0,0 +1,19 @@
+package DAO;
+
+import Model.City;
+
+import java.util.Map;
+
+public interface CityDAO {
+
+
+	boolean addCity(City city);
+
+	City findByID(int id);
+
+	Map<Integer, String> getAll();
+
+	boolean updateByID(int id, City city);
+
+	boolean deleteByID(int id);
+}
